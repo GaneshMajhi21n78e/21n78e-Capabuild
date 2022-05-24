4fccad7c3ea9d44102cfdb7b714652c5aaaca221
manan427 <modimanan00@gmail.com>
2022-05-24 12:06:35 +0530

    added carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeaderCarouselComponent } from './components/header-carousel/header-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderCarouselComponent
   ],
   imports: [
     BrowserModule,
